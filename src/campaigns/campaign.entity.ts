9dada0a445f0f4d7fd9246d61952ece616e54704
sidhardev <your_email@example.com>
2026-01-07 12:06:54 +0530

    Created Campaign Entity

---
+++ src/campaigns/campaign.entity.ts
@@ -0,0 +1,64 @@
+import { IsOptional } from "class-validator";
+import { timestamp } from "rxjs";
+import { PrimaryGeneratedColumn, Column,  Entity, CreateDateColumn, UpdateDateColumn } from "typeorm";
+
+export enum CampaignType {
+    ORDER_DISCOUNT = "ORDER_DISCOUNT",
+    REWARD = "REWARD",
+    FREE_SHIPPING = "FREE_SHIPPING",
+}
+
+export enum CampaignStatus {
+    DRAFT = "DRAFT",
+    ACTIVE = "ACTIVE",
+    PAUSED = "PAUSED",
+    EXPIRED = "EXPIRED"
+}
+
+
+@Entity('campaigns')
+
+export class campaign {
+    @PrimaryGeneratedColumn() 
+    id: number;
+
+    @Column() 
+    name: string;
+
+    @Column()
+    @IsOptional()
+    description: string;
+
+    @Column({
+        type: 'enum',
+        enum: CampaignStatus,
+        default: CampaignStatus.DRAFT
+    })
+    status: CampaignStatus;
+
+    @Column({
+        type: 'enum',
+        enum: CampaignType,
+        
+    })
+    type: CampaignType;
+
+    @Column({type: 'timestamp'})
+    startAt: Date;
+
+    @Column({type: "timestamp"})
+    endAt: Date;
+
+    @Column({type: 'jsonb', nullable: true})
+    metadata: Record<string, any>;
+
+    @CreateDateColumn()
+    createdAt: Date;
+
+    @UpdateDateColumn()
+    updatedAt: Date;
+
+
+
+
+}
