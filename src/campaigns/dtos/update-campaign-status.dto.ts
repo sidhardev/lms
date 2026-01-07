eaf93a00039c6aa062cd835ddfc12ced0f1e7f2b
Dev Sidhar <devsuthar065@gmail.com>
2026-01-07 12:56:28 +0530

    Update campaig status created sucessfully

---
+++ src/campaigns/dtos/update-campaign-status.dto.ts
@@ -0,0 +1,12 @@
+import { IsEnum, } from "class-validator";
+import { CampaignStatus } from "../campaign.entity";
+import { ApiProperty } from "@nestjs/swagger";
+
+export class UpdateCampaignStatusDto {
+    @IsEnum(CampaignStatus)
+    @ApiProperty({
+        example: 'ACTIVE'
+    })
+    status: CampaignStatus;
+    
+}
