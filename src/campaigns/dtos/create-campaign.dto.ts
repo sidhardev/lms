d5f9edd8ca909d341c695dc0de7e71466c043f4b
Dev Sidhar <devsuthar065@gmail.com>
2026-01-07 12:21:01 +0530

    Created Create Campaign Dto Final Version!

--- src/campaigns/dtos/create-campaign.dto.ts
+++ src/campaigns/dtos/create-campaign.dto.ts
@@ -1,2 +1,36 @@
-import { IsString, IsNumber, IsEnum } from "class-validator";
+import { IsString, IsNumber, IsEnum, IsOptional, IsDateString } from "class-validator";
 import { CampaignType } from "../campaign.entity";
+import { ApiProperty } from "@nestjs/swagger";
+
+export class CreateCampaignDto {
+
+    @IsString()
+    @ApiProperty({
+        example: 'Diwali Festive Offers 2026'
+    })
+    name: string;
+
+    @IsOptional()
+    @IsString()
+    @ApiProperty({
+        example: 'Give users Diwali offer 2026 that increase our sales!'
+    })
+    description: string;
+
+    @IsEnum(CampaignType)
+    type: CampaignType;
+
+    @IsDateString()
+    startAt: Date;
+
+    @IsDateString()
+    endAt: Date;
+
+
+    @IsOptional() 
+    metadata: Record<string, any>
+
+
+
+
+}
